6027d95e92e7716414bb3d81375d53701279e75f
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-04 20:33:18 +0900

    Title: 문자 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int repeat = 8;
+
+    char a;
+    cin >> a;
+
+    for (int i=0;i<repeat;i++){
+        cout<<a;
+    }
+    return 0;
+}
