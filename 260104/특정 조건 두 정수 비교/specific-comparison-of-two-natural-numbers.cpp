cd362bef90a78ea79ba935fab47edecbabb5e057
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-04 20:17:22 +0900

    Title: 특정 조건 두 정수 비교, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    bool flag1 = (a<b);
+    bool flag2 = (a==b);
+
+    cout << flag1 << " " << flag2 << endl;
+    return 0;
+}
