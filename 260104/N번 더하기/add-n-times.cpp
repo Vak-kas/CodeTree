580c445c539f1d3eb24c7bff2b44bc70ff3df30f
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-04 20:38:27 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/N번 더하기/add-n-times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, n;
+    cin >> a >> n;
+
+    int total = a;
+    for (int i=0;i<n;i++){
+        total = total + n;
+        cout << total << endl;
+
+    }
+    return 0;
+}
