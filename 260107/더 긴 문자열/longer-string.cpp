0269c1d9d78d84e2a4d2c102caaf46f3dcabd1ca
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-07 09:25:28 +0900

    Title: 더 긴 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a, b;
+
+    cin >> a >> b;
+
+    if(a.length() == b.length()){
+        cout << "same";
+    }
+    else{
+        if(a.length() > b.length()){
+            cout << a << " " << a.length();
+        }
+        else{
+            cout << b << " " << b.length();
+        }
+    }
+    return 0;
+}
