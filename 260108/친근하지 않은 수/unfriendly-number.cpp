26874b5b73c02235e3fd564516dfcb4d04e48196
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-08 12:56:46 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260108/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    cin >> n;
+
+    int count = 0;
+    for(int i=1;i<=n;i++){
+        if(i%2==0){
+            continue;
+        }
+        
+        if(i%3 == 0){
+            continue;
+        }
+
+        if(i%5 == 0){
+            continue;
+        }
+
+        count+=1;
+
+    }
+
+    cout << count << endl;
+    return 0;
+}
