1908900bb16705339264ab6d0b015629a993728b
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-16 00:17:53 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260116/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[4][4];
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int count = 0;
+
+    for(int i=0;i<4;i++)
+    {
+       for(int j=0;j<i+1;j++)
+       {
+        count += arr[i][j];
+       }
+    }
+
+    cout << count << endl;
+    return 0;
+}
