51cb9df2ed5456e2c43630a48a44d817b86ca5a2
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-19 08:33:00 +0900

    Title: 변수 값 교체하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 5;
+    int tmp = a;
+    a = b;
+    b = tmp;
+
+    cout << a<<endl;
+    cout << b << endl;
+    return 0;
+}
