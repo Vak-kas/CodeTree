a390c6c4efa46bff94ef74ed09f6016ab52e95cc
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-18 12:04:54 +0900

    Title: 데이터 교환, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/데이터 교환/exchange-data.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    int a_ = a;
+    int b_ = b;
+    int c_ = c;
+
+
+
+    b = a_;
+    c = b_;
+    a = c_;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    
+
+    return 0;
+}
