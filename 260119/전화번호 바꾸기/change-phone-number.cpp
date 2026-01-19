1265e905006f0f35d8d4a92adfd4bdc95d4e1042
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-19 09:14:43 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int x, y;
+
+    cin >> x;
+    // cin.get();
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+
+    cout << "010-" << y << "-" << x;
+
+    return 0;
+}
