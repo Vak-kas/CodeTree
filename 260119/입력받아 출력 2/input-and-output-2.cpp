2fe2e49da81f70ae9f98440dfc706e7f851b024f
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-19 09:06:05 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int front, back;
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << front << back << endl;
+    return 0;
+}
