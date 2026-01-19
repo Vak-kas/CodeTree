8a36529d4adc2387f804cb20c6f540796b9a1737
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-19 09:05:10 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260119/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int year, month, day;
+    cin >> month;
+    cin.get();
+    cin >> day;
+    cin.get();
+    cin >> year;
+
+    cout << year << "." << month << "." << day << endl;
+    return 0;
+}
