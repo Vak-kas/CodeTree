1745a0fbc41fcfe0eb27eef33c8817de981fabad
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-23 23:26:03 +0900

    Title: 알파벳에 따른 평가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260123/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char n;
+    cin >> n;
+
+    if(n == 'S'){
+        cout << "Superior" << endl;
+    }
+    else if(n == 'A'){
+        cout << "Excellent" << endl;
+    }
+    else if(n == 'B'){
+        cout << "Good" << endl;
+    }
+    else if(n == 'C'){
+        cout << "Usually" << endl;
+    }
+    else if(n == 'D'){
+        cout << "Effort" << endl;
+    }
+    else{
+        cout << "Failure" << endl;
+
+    }
+    return 0;
+}
