23e9f59d2f7cd994bbb8f9c9cc69e081b6868915
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-23 20:24:22 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260123/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int score;
+    cin >> score;
+
+    bool flag = score == 100 ? true : false;
+
+    if(true){
+        cout << "pass" << endl;
+    }
+    else{
+        cout << "failure" << endl;
+    }
+    return 0;
+    
+}
