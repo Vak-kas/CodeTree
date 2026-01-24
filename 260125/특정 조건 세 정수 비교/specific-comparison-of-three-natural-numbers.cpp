720a02a42db3156f487d643242d25db182805a54
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-25 00:35:06 +0900

    Title: 특정 조건 세 정수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260125/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if((a<=b) && (a<=c)){
+        cout << 1 << " " ;
+    }
+    else{
+        cout << 0 << " ";
+    }
+
+    if((a==b) && (a==c)){
+        cout << 1 << endl;
+    }
+    else{
+        cout << 0 << endl;
+    }
+    return 0;
+}
