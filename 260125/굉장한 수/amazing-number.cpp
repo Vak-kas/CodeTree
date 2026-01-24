412aece7dfd90a8fdf1d0c36e9f2127046e23bc2
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-25 00:57:52 +0900

    Title: 굉장한 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260125/굉장한 수/amazing-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    if(((n%2==1) && (n%3 == 0)) || (n%2==0) && (n%5 == 0)){
+        cout <<  "true" << endl;
+    }
+    else{
+        cout << "false" << endl;
+    }
+    return 0;
+}
