5d51e104ba2f18f84611f6486970b2ec3df6aad3
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-26 11:56:35 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260126/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i = 1, n;
+    cin >> n;
+
+    while(i<=n){
+        cout << i  << " ";
+        i++;
+    }
+    return 0;
+}
