7b7c11292e8e3f165e97ed6e8545fd660ee7d84f
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-28 22:33:30 +0900

    Title: 감소시키며 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i=26;
+    while(true){
+        cout << i << " ";
+        i--;
+
+        if(i<10){
+            break;
+        }
+    }
+    return 0;
+}
