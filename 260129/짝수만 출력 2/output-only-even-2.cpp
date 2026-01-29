eabad7e4cbc9cac6f3f5334381a8cd3c8b8846b7
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-01-29 15:36:20 +0900

    Title: 짝수만 출력 2, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260129/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> b >> a;
+
+    while (b>=a){
+        cout << b << " ";
+        b-=2;
+    }
+    return 0;
+}
