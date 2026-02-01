ab12c5bcc6f2c6b5d58614c9795c6cdba146234d
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-01 23:08:41 +0900

    Title: 배수의 개수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260201/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int cnt = 0;
+    for(int i=0;i<10;i++){
+        int tmp ;
+        cin >> tmp;
+
+        if(tmp%2 == 1){
+            cnt++;
+        }
+
+
+    }
+    cout << cnt << endl;
+    return 0;
+}
