e032945a0c11d28e08f54c33b4dfc1439daea48c
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-03 13:57:16 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int i=n;i<=100;i++){
+        sum+=i;
+
+    }
+
+    cout << sum << endl;
+    return 0;
+}
