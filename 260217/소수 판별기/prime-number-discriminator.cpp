25d623ec7593ee4a730ebacf6ad3bf0236b6aab9
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-17 09:56:03 +0900

    Title: 소수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260217/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    bool flag = true;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag){
+        cout << "P";
+    }
+    else{
+        cout << "C";
+    }
+    return 0;
+}
