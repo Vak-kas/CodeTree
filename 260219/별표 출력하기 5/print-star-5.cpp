1e28b7250e9fcd762e218afcc474e510aaab2629
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-19 11:36:17 +0900

    Title: 별표 출력하기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260219/별표 출력하기 5/print-star-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=n;i>0;i--){
+        for(int j=1;j<=i;j++){
+            for(int k=0;k<i;k++){
+                cout << "*";
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
