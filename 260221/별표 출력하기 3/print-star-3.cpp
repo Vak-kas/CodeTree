6080c94ce2dfd4ca7d3694f2eec2550c568ed32d
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-21 00:10:32 +0900

    Title: 별표 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260221/별표 출력하기 3/print-star-3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=n;i>=1;i--){
+        for(int j=0;j<2*(n-i);j++)
+        {
+            cout << " ";
+        }
+        for(int j=0;j<2*i-1;j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
