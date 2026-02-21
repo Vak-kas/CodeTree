78122a34e3a743cfbf2e040fcd7228894d8e4f96
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-22 00:22:30 +0900

    Title: 두 개의 직각삼각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260222/두 개의 직각삼각형/two-right-triangle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=n;j++){
+            cout << "*";
+        }
+        for(int j=1;j<i;j++){
+            cout << " ";
+        }
+
+        for(int j=1;j<i;j++){
+            cout << " ";
+        }
+
+        for(int j=i;j<=n;j++){
+            cout << "*";
+        }
+
+        
+        cout << endl;
+    }
+    return 0;
+}
