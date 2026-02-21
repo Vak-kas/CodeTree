e53cedf2eaa7cd6084d5f2f8aea97db83fab250a
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-21 23:48:02 +0900

    Title: 별표 출력하기 9, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260221/별표 출력하기 9/print-star-9.cpp
+++ 260221/별표 출력하기 9/print-star-9.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    // Please write your code here.
+    // 변수 선언 및 입력
     int n;
     cin >> n;
 
-    for(int i=0;i<n;i++){
-        for(int j=n;j>(2*(i-1));j--){
-            cout << " ";
-        }
-        for(int j=0;j<2*i+1; j++){
-            cout << "* ";
-        }
-        cout << endl;
+    // 모양에 맞게 별을 출력합니다.
+	for(int i = 0; i < n; i++) {
+		for(int j = n - i; j > 1; j--)
+			cout << "  ";
+		for(int j = 0; j < 2 * i + 1; j++)
+			cout << "* ";
+		cout << endl;
     }
+	
     return 0;
 }
