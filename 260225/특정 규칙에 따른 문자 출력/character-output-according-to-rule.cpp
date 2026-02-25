7232c086922e7321bb8a6f5dadbb75977b337d34
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-25 14:06:13 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260225/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+
+        for(int j =0; j < (2*(n-1-i)) ;j++){
+            cout << " ";
+        }
+    
+        for(int j=0;j<i+1;j++){
+            cout << "@ ";
+        }
+
+        cout << endl;
+    }
+
+    for(int i=n-1;i>0;i--){
+    
+        for(int j=0;j<i;j++){
+            cout << "@ ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
