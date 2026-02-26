275eb370eca106a5c4f695784ece13f1a70b38ec
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-02-26 21:29:12 +0900

    Title: 체크 다이아몬드, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260226/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>i;j--){
+            cout << " ";
+        }
+
+        for(int j=0;j<=i;j++){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    for(int i=n-2;i>-1;i--){
+        for(int j=n-1;j>i;j--){
+            cout << " ";
+        }
+
+        for(int j=0;j<=i;j++){
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
