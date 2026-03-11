ecfd16630b85453b2bdd87e37aa72558ac120b58
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-03-11 09:56:17 +0900

    Title: 구구단 만들기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260311/구구단 만들기/print-multiplication-table.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    for(int i = 1; i<=9 ; i++){
+        for(int j=b ; j>=a ; j-=2){
+            cout << j << " * " << i << " = " << i*j;
+            if(j != 2 ){
+                cout << " / ";
+            }
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
