55a358c712121fac844a8adc2d0fb3836f8bfa78
서민재 <110338700+Vak-kas@users.noreply.github.com>
2026-03-25 09:02:45 +0900

    Title: 알파벳 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260325/알파벳 출력하기 2/print-alphabet-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    char k = 'A';
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i<=j){
+                cout << k << " ";
+                k++;
+                if(k>'Z'){
+                    k='A';
+                }
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
